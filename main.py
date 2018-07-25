36abd0c4abf93de4ef0e6d65b674b67d1d884b67
WeaverDyl <dweaver@umd.edu>
2018-07-25 18:56:05 -0400

    add morse code module

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bomb import Bomb
 from simple_wires import SimpleWires
+from morse_code import MorseCode
 
 def main():
     bomb = Bomb() # Create a new bomb instance, which stores indicator/battery/other info
@@ -7,11 +8,15 @@ def main():
     # Pick a module
     while True:
         curr_module = input("What module would you like to solve? Your options are:\n\n"\
-                            "Simple Wires\n\n")
+                            "Simple Wires / Morse Code\n\n")
 
         if curr_module.lower() == 'simple wires':
             simple_wires = SimpleWires()
             simple_wires.start_simple_wires(bomb)
+            
+        if curr_module.lower() == 'morse code':
+            morse_code = MorseCode()
+            morse_code.start_morse_code()
 
 if __name__ == "__main__":
     main() # Begin!
---
+++ morse_code.py
@@ -0,0 +1,65 @@
+class MorseCode:
+    frequencies = {
+        "shell": 505,
+        "halls": 515,
+        "slick": 522,
+        "trick": 532,
+        "boxes": 535,
+        "leaks": 542,
+        "strobe": 545,
+        "bistro": 552,
+        "flick": 555,
+        "bombs": 565,
+        "break": 572,
+        "brick": 575,
+        "steak": 582,
+        "sting": 592,
+        "vector": 595,
+        "beats": 600
+    }
+
+    morse_code_lookup = {
+        '.-'   : 'a',
+        '-...' : 'b',
+        '-.-.' : 'c',
+        '-..'  : 'd',
+        '.'    : 'e',
+        '..-.' : 'f',
+        '--.'  : 'g',
+        '....' : 'h',
+        '..'   : 'i',
+        '.---' : 'j',
+        '-.-'  : 'k',
+        '.-..' : 'l',
+        '--'   : 'm',
+        '-.'   : 'n',
+        '---'  : 'o',
+        '.--.' : 'p',
+        '--.-' : 'q',
+        '.-.'  : 'r',
+        '...'  : 's',
+        '-'    : 't',
+        '..-'  : 'u',
+        '...-' : 'v',
+        '.--'  : 'w',
+        '-..-' : 'x',
+        '-.--' : 'y',
+        '--..' : 'z'
+    }
+
+    def start_morse_code(self):
+        # Input the actual dots and dashes
+        morse_code = input("Input each morse code character seperated by a space. ")
+        # Convert the morse characters into letters by looking each character up
+        word = [self.morse_code_lookup[x] for x in morse_code.split(' ')]
+        # Combine the list of letters into the word!
+        result = ''.join(word)
+
+        try:
+            # Print the resulting frequency if it exists
+            print(f"Respond at frequency 3.{self.frequencies[result]}")
+            return
+        except KeyError:
+            # The user interpreted the morse code incorrectly
+            print("An error occurred! Please try again.")
+            self.start_morse_code()
