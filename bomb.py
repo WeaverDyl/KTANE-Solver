6e0cb524616c48369332f8abeb78f82ecc750e05
WeaverDyl <dweaver@umd.edu>
2018-07-26 00:27:09 -0400

    commit rest of whos on first additions

--- bomb.py
+++ bomb.py
@@ -5,10 +5,13 @@ class Bomb:
         self.serial_no = bomb_data[0] # The serial number on the bomb
         self.batteries = bomb_data[1] # The number of batteries contained in the bomb
         self.indicators = bomb_data[2] # A list of all lit indicators on the bomb
+        self.ports = bomb_data[3] # All of the ports on the bomb
+        self.strikes = 0 # The number of strikes can affect diffusal
 
     def collect_info(self):
         serial_no = input("What's the serial number on the bomb? ")
         batteries = input("How many batteries are there on the bomb? ")
         indicators = [i.strip() for i in input("Enter a comma-seperated list of lit indicators on the bomb. ").split(',')]
+        ports = [i.strip() for i in input("Enter a comma-seperated list of ports on the bomb. ").split(',')]
 
-        return serial_no, batteries, indicators
+        return serial_no, batteries, indicators, ports
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bomb import Bomb
 from game_modules.simple_wires import SimpleWires
 from game_modules.morse_code import MorseCode
+from game_modules.passwords import Passwords
+from game_modules.whos_on_first import WhosOnFirst
 
 def main():
     bomb = Bomb() # Create a new bomb instance, which stores indicator/battery/other info
@@ -8,7 +10,7 @@ def main():
     # Pick a module
     while True:
         curr_module = input("What module would you like to solve? Your options are:\n\n"\
-                            "Simple Wires / Morse Code\n\n")
+                            "Simple Wires / Morse Code / Passwords / Who's on First\n\n")
 
         if curr_module.lower() == 'simple wires':
             simple_wires = SimpleWires()
@@ -17,6 +19,15 @@ def main():
         if curr_module.lower() == 'morse code':
             morse_code = MorseCode()
             morse_code.start_morse_code()
+            
+        if curr_module.lower() == 'passwords':
+            passwords = Passwords()
+            passwords.start_passwords()
+
+        if curr_module.lower() == 'who\'s on first':
+            print("Good")
+            whos_on_first = WhosOnFirst()
+            whos_on_first.start_whos_on_first(bomb, 0)
 
 if __name__ == "__main__":
     main() # Begin!
